f9dc20a4ed3d6f8c577b67cb168b457cf1ecb184
monkviper <surinder@anattadesign.com>
2017-05-31 07:43:01 +0530

    working on homepage

--- assets/theme.js
+++ assets/theme.js
@@ -1241,7 +1241,7 @@ theme.Slideshow = (function() {
 
     this.settings = {
       accessibility: true,
-      arrows: false,
+      arrows: true,
       dots: true,
       fade: true,
       draggable: true,
