bde6a357fb3c3c926e8c5138f615d75a8ff2b670
monkviper <surinder@anattadesign.com>
2017-06-12 20:49:55 +0530

    add anatta frontend toolkit

---
+++ anattafrontendkit/project.config.js
@@ -0,0 +1,16 @@
+/**
+ * Project info config object
+ *
+ */
+module.exports = {
+	name: 'Project name',
+	description: 'Project description',
+	version: 'Project version',
+	author: 'Project author',
+	authorURI: 'Project author website',
+
+    buildDirName: 'dist',
+    buildDirPath: './dist',
+    srcDirPath: './src',
+    browserSyncProxy: 'anatta.toolkit',
+};
