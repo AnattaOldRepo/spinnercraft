1106c2888de92ddf36556bc2ff92c1a95170d3b3
maverickmj <maninder@anattadesign.com>
2017-07-21 21:49:32 +0530

    completed the styling of marketing page

--- assets/theme.js
+++ assets/theme.js
@@ -1287,6 +1287,32 @@ $('.js-blogSlider').slick({
     ]
 });
 
+
+if (window.matchMedia('(min-width: 1024px)').matches) {
+  $('.js-marketSlider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerPadding: '40px',
+      dots: false,
+      arrows: true,
+      centerMode: true,
+      variableWidth: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            centerPadding: '30px',
+            slidesToScroll: 1,
+            variableWidth: false,
+            arrows: true
+          }
+        }
+       
+      ]
+  });
+}
+
 (function() {
   var selectors = {
     backButton: '.return-link'
